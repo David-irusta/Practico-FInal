38a45c17e68ae04ea6f7da045cf14d98c0438f4f
David <david.irusta@itscordoba.edu.ar>
2025-10-23 18:43:36 -0300

    Url's de ventas

--- inventario/ventas/urls.py
+++ inventario/ventas/urls.py
@@ -2,3 +2,15 @@ from django.urls import path
 from . import views
 
 app_name = 'ventas'
+
+urlpatterns = [
+    path('crear_venta/',
+         views.CrearVentaView.as_view(), 
+         name='crear_venta'),
+    path('<int:pk>/detalle_venta/',
+         views.VentaDetailView.as_view(), 
+         name='detalle_venta'),
+    path('lista_ventas/',
+         views.VentaListView.as_view(),
+            name='lista_ventas'),
+]
